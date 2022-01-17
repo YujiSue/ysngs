7fcdddaaf92bc87165c6fa0bd8239fd0ac132391
Yuji Suehiro <yuji.sue@gmail.com>
2022-01-17 14:24:57 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ysngs',
+	version='0.1',
+	author='Yuji Suehiro',
+	packages=find_packages(),
+	url='https://github.com/YujiSue/ysngs',
+	description='Support functions for NGS data analysis.'
+)
