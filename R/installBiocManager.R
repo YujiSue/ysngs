bb9816a2aabf65e7d60866b1d905a77747d5f221
Yuji Suehiro <yuji.sue@gmail.com>
2023-05-02 15:30:39 +0900

    Add files via upload

--- R/installBiocManager.R
+++ R/installBiocManager.R
@@ -1,3 +1,5 @@
+args <- commandArgs(trailingOnly = T)
+repos_url=""
+if (1 < length(args)) { repos_url=args[1] } else { repos_url="http://cran.us.r-project.org" }
 if (!requireNamespace("BiocManager"))
-    install.packages("BiocManager", repos = "http://cran.us.r-project.org")
-print(BiocManager::version())
+    install.packages("BiocManager", repos = repos_url)
